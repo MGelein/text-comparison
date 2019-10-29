f7e790828e2834d6036f128e06b46c9ff45f9fb5
Mees Gelein <meesgelein@gmail.com>
2019-10-29 10:46:41 +0100

    Added duplicate prevention

--- src/textcomparison.js
+++ src/textcomparison.js
@@ -1,4 +1,4 @@
-const {getFilesFromDescriptors, exportSave} = require("./file");
+const { getFilesFromDescriptors, exportSave } = require("./file");
 //Saves the results of the comparison for each file
 const matches = {};
 //the files used in this comparison
@@ -18,7 +18,7 @@ var fullText = false;
  * Starts a comparison as described by the passed-on settings object
  * @param {Object} settings the settings object as passed on from the index.js
  */
-function doComparison(settings){
+function doComparison(settings) {
     ngram = settings.ngram;
     lowercase = settings.lowercase;
     whitespace = settings.whitespace;
@@ -33,18 +33,18 @@ function doComparison(settings){
  * into this comparison list of files
  * @param {Array} list 
  */
-function setFiles(list){
+function setFiles(list) {
     //Remove all old elements
-    while(files.length > 0) files.splice(1);
+    while (files.length > 0) files.splice(1);
     //Add all the new ones
-    for(file of list){
-        if(lowercase) file.contents = file.contents.toLowerCase();
-        if(!whitespace) file.contents = file.contents.replace(/\s/g, '');
+    for (file of list) {
+        if (lowercase) file.contents = file.contents.toLowerCase();
+        if (!whitespace) file.contents = file.contents.replace(/\s/g, '');
         files.push(file);
     }
     matches.ids = [];
     matches.passages = [];
-    for(file of files){
+    for (file of files) {
         matches.ids.push(file.desc);
     }
     //Now that we have files, create dictionaries
@@ -58,31 +58,31 @@ function setFiles(list){
 /**
  * Creates the ngram dictionaries that keep track of each ngram and its occurences for each text
  */
-function createDicts(){
+function createDicts() {
     //Do this for each file
-    for(let i = 0; i < files.length; i++){
+    for (let i = 0; i < files.length; i++) {
         let file = files[i];
         console.log("Creating dictionary " + (file.id + 1) + " of " + files.length + "... " + file.desc);
         file.dict = createDict(file.contents);
         let keys = Object.keys(file.dict);
-        let record = {name: "", num: 0};
-        for(key of keys){
-            if(file.dict[key].length > record.num){
+        let record = { name: "", num: 0 };
+        for (key of keys) {
+            if (file.dict[key].length > record.num) {
                 record.name = key;
                 record.num = file.dict[key].length;
             }
         }
         console.log("\tThis text has a total of " + keys.length + " unique ngrams. Most common: " + record.name
-        + " (" + record.num + " occurences).");
+            + " (" + record.num + " occurences).");
     }
 }
 
 /**
  * Compare all the files to every other file
  */
-function compareAllFiles(){
-    for(let a = 0; a < files.length; a++){
-        for(let b = a + 1; b < files.length; b++){
+function compareAllFiles() {
+    for (let a = 0; a < files.length; a++) {
+        for (let b = a + 1; b < files.length; b++) {
             compareFiles(files[a], files[b]);
         }
     }
@@ -93,7 +93,7 @@ function compareAllFiles(){
  * @param {File} fileA 
  * @param {File} fileB 
  */
-function compareFiles(fileA, fileB){
+function compareFiles(fileA, fileB) {
     console.log("------------------------------");
     console.log("Starting comparison between " + fileA.desc + " and " + fileB.desc);
     //Find the shared n-grams
@@ -101,9 +101,9 @@ function compareFiles(fileA, fileB){
     let keysB = new Set(Object.keys(fileB.dict));
 
     let keysShared = Array.from(
-      new Set(keysA.filter(key => keysB.has(key)))
+        new Set(keysA.filter(key => keysB.has(key)))
     );
-    
+
     console.log("\tThese files share " + keysShared.length + " ngrams.");
     console.log("Starting expansion of shared ngrams...");
     expandSharedKeys(fileA, fileB, keysShared);
@@ -115,14 +115,14 @@ function compareFiles(fileA, fileB){
  * @param {File} fileB 
  * @param {Array} keysShared 
  */
-function expandSharedKeys(fileA, fileB, keysShared){
+function expandSharedKeys(fileA, fileB, keysShared) {
     //For each of the shared keys, find the occruences in both texts
-    for(let sharedKey of keysShared){
+    for (let sharedKey of keysShared) {
         let occsA = fileA.dict[sharedKey];
         let occsB = fileB.dict[sharedKey];
         //Each occurence in text A needs to be compared to each occurence in text B
-        for(occA of occsA){
-            for(occB of occsB){
+        for (occA of occsA) {
+            for (occB of occsB) {
                 expandSharedKey(fileA, fileB, occA, occB);
             }
         }
@@ -136,7 +136,7 @@ function expandSharedKeys(fileA, fileB, keysShared){
  * @param {Integer} indexA 
  * @param {Integer} indexB 
  */
-function expandSharedKey(fileA, fileB, indexA, indexB){
+function expandSharedKey(fileA, fileB, indexA, indexB) {
     const MAX_STRIKES = 5;
     let strikes = MAX_STRIKES;
     let textA = fileA.contents;
@@ -146,28 +146,28 @@ function expandSharedKey(fileA, fileB, indexA, indexB){
     let sim, prevSim = 1;//Previous similarity
     let leftChecked = false;
     //Try expanding as long as the expansion fits in the actual text
-    while(indexA > 0 && indexB > 0 && indexA + matchL < textA.length && indexB + matchL < textB.length){
+    while (indexA > 0 && indexB > 0 && indexA + matchL < textA.length && indexB + matchL < textB.length) {
         //First grab the two parts
         partA = textA.substr(indexA, matchL);
         partB = textB.substr(indexB, matchL);
         sim = getSimilarity(partA, partB);
 
         //Expand to the correct side
-        if(leftChecked){
-            matchL ++;
-        }else{
-            indexA --;
-            indexB --;
+        if (leftChecked) {
+            matchL++;
+        } else {
+            indexA--;
+            indexB--;
         }
 
         //See if the match is improving or not
-        if(sim < prevSim) strikes--;
-        else if(sim > prevSim && strikes < MAX_STRIKES) strikes ++;
+        if (sim < prevSim) strikes--;
+        else if (sim > prevSim && strikes < MAX_STRIKES) strikes++;
 
         //If the strikes reach 0, either expand to the other side, or just quit now
-        if(strikes == 0){
-            if(leftChecked) break;
-            else{
+        if (strikes == 0) {
+            if (leftChecked) break;
+            else {
                 leftChecked = true;
                 strikes = MAX_STRIKES;
             }
@@ -186,19 +186,30 @@ function expandSharedKey(fileA, fileB, indexA, indexB){
  * @param {Integer} indexB 
  * @param {Integer} matchL 
  */
-function registerMatch(fileA, fileB, indexA, indexB, matchL){
+function registerMatch(fileA, fileB, indexA, indexB, matchL) {
     //What is the ID of this comparison
     let compId = fileA.id + "_" + fileB.id;
     //If the array of matches is not there yet, make it
-    if(matches[compId] == undefined) matches[compId] = [];
+    if (matches[compId] == undefined) matches[compId] = [];
     //Finally register the new match
     let passageA = fileA.id + "@" + indexToMarker(fileA.contents, indexA) + "-" + indexToMarker(fileA.contents, indexA + matchL);
     let passageB = fileB.id + "@" + indexToMarker(fileB.contents, indexB) + "-" + indexToMarker(fileB.contents, indexB + matchL);
-    if(!fullText) matches.passages.push([passageA, passageB]);
-    else {
-        matches.passages.push([
-            passageA, fileA.contents.substr(indexA, matchL), passageB, fileB.contents.substr(indexB, matchL)
-        ]);
+    //See if we have an already matching passage
+    let isNewMatch = true;
+    for (let passage of matches.passages) {
+        if (passage[0] === passageA && passage[fullText ? 2 : 1] === passageB) {
+            isNewMatch = false;
+            break;
+        }
+    }
+    //Only add the new match if it is not a duplicate of what we already have
+    if (isNewMatch) {
+        if (!fullText) matches.passages.push([passageA, passageB]);
+        else {
+            matches.passages.push([
+                passageA, fileA.contents.substr(indexA, matchL), passageB, fileB.contents.substr(indexB, matchL)
+            ]);
+        }
     }
 }
 
@@ -208,12 +219,12 @@ function registerMatch(fileA, fileB, indexA, indexB, matchL){
  * @param {String} text 
  * @param {Integer} index 
  */
-function indexToMarker(text, index){
+function indexToMarker(text, index) {
     let char = text.charAt(index);
     let counter = 0;
     let prevText = text.substring(0, index);
-    for(let c of prevText){
-        if(c === char) counter ++;
+    for (let c of prevText) {
+        if (c === char) counter++;
     }
     return char + "[" + counter + "]";
 }
@@ -225,10 +236,10 @@ function indexToMarker(text, index){
  * @param {String} stringA 
  * @param {String} stringB 
  */
-function getSimilarity(stringA, stringB){
+function getSimilarity(stringA, stringB) {
     let same = 0;
-    for(let i = 0; i < stringA.length; i++){
-        if(stringA.charAt(i) === stringB.charAt(i)) same++;
+    for (let i = 0; i < stringA.length; i++) {
+        if (stringA.charAt(i) === stringB.charAt(i)) same++;
     }
     return same / stringA.length;
 }
@@ -237,14 +248,14 @@ function getSimilarity(stringA, stringB){
  * Creates a dictionairy of ngrams from the provided text
  * @param {String} text 
  */
-function createDict(text){
+function createDict(text) {
     let dict = {};
     let gram;
-    for(let i = 0; i < text.length - ngram; i++){
+    for (let i = 0; i < text.length - ngram; i++) {
         gram = text.substr(i, ngram);
-        if(dict[gram] == undefined){//This means this gram was not yet registered
+        if (dict[gram] == undefined) {//This means this gram was not yet registered
             dict[gram] = [i];//Add a new array with the first occurence
-        }else{
+        } else {
             dict[gram].push(i);//Register this index as another occurence of this gram
         }
     }
